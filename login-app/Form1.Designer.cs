1471a53578e34cad6ba57ba833ce9a7bb68a0e6a
Mark <Mark@ANNONYMOUS-PC>
2023-11-17 21:28:15 -0800

    Add project files.

---
+++ login-app/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿
+namespace login_app
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbCaptured = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.user = new System.Windows.Forms.TextBox();
+            this.pass = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.fname = new System.Windows.Forms.TextBox();
+            this.lname = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCaptured)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbCaptured
+            // 
+            this.pbCaptured.Location = new System.Drawing.Point(12, 12);
+            this.pbCaptured.Name = "pbCaptured";
+            this.pbCaptured.Size = new System.Drawing.Size(567, 344);
+            this.pbCaptured.TabIndex = 0;
+            this.pbCaptured.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 362);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(567, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 411);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // user
+            // 
+            this.user.Location = new System.Drawing.Point(647, 249);
+            this.user.Name = "user";
+            this.user.Size = new System.Drawing.Size(147, 20);
+            this.user.TabIndex = 7;
+            // 
+            // pass
+            // 
+            this.pass.Location = new System.Drawing.Point(647, 303);
+            this.pass.Name = "pass";
+            this.pass.Size = new System.Drawing.Size(147, 20);
+            this.pass.TabIndex = 8;
+            this.pass.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(644, 233);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Username: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(644, 287);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Password: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(644, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Firstname";
+            // 
+            // fname
+            // 
+            this.fname.Location = new System.Drawing.Point(647, 132);
+            this.fname.Name = "fname";
+            this.fname.Size = new System.Drawing.Size(147, 20);
+            this.fname.TabIndex = 12;
+            // 
+            // lname
+            // 
+            this.lname.Location = new System.Drawing.Point(647, 185);
+            this.lname.Name = "lname";
+            this.lname.Size = new System.Drawing.Size(147, 20);
+            this.lname.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(644, 169);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Lastname";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(641, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Register";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(641, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Login";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Admin",
+            "Staff"});
+            this.comboBox1.Location = new System.Drawing.Point(647, 352);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 18;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(647, 337);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 13);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Role";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lname);
+            this.Controls.Add(this.fname);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pass);
+            this.Controls.Add(this.user);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pbCaptured);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbCaptured)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbCaptured;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox user;
+        private System.Windows.Forms.TextBox pass;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox fname;
+        private System.Windows.Forms.TextBox lname;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ login-app/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FaceRecognition;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Face;
+
+namespace login_app
+{
+    public partial class Form1 : Form
+    {
+        DataClasses1DataContext db = new DataClasses1DataContext();
+        string full;
+        string uname;
+        string pword;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+           
+        }
+        FaceRec face = new FaceRec();           
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Hide();
+            face.isTrained = true;
+            face.openCamera(pbCaptured, pbCaptured);
+            face.getPersonName(label1);
+            
+            
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int role = (int)comboBox1.SelectedIndex;
+            db.register(fname.Text, lname.Text, user.Text, pass.Text, role);
+            face.Save_IMAGE(fname.Text +" "+lname.Text);
+            MessageBox.Show("Save successfully!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var name = db.getAcc(label1.Text).ToList();
+            if(name != null && name.Any())
+            {
+                foreach(var item in name)
+                {
+                    full = item.Fullname;
+                }
+            }
+
+            var up = db.sp_login(user.Text, pass.Text).ToList();
+            if(up!=null && up.Any())
+            {
+                foreach(var item in up)
+                {
+                    uname = item.username;
+                    pword = item.pass;
+                }
+            }
+
+           
+
+            try
+            {
+                if (full == label1.Text)
+                {
+                    
+                    if(db.sp_type(uname,pword) == 0)
+                    {
+                        MessageBox.Show($"Hi: {full} Welcome Admin!");
+                    }
+                    else
+                    {
+                        MessageBox.Show($"Hi: {full} Welcome Staff!");
+                    }
+                }
+                else if (uname == user.Text && pword == pass.Text)
+                {
+                    if (db.sp_type(uname, pword) == 0)
+                    {
+                        MessageBox.Show($"Hi: {full} Welcome Admin!");
+                    }
+                    else
+                    {
+                        MessageBox.Show($"Hi: {full} Welcome Staff!");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("SAYOP KA BOGO UNDANG SKWELA!");
+                }
+            }catch(Exception ex)
+            {
+                MessageBox.Show("Didn't detect your face!");
+            }
+        }
+    }
+}
